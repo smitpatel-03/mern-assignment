a2a9cb4183093f76fc3cfcd6adfee570d8fb18af
smitpatel-03 <patelsmitkumar03@gmail.com>
2022-06-25 12:22:18 +0530

    ♻minor changes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -9,8 +9,8 @@ const {
 const userRouter = express.Router();
 
 userRouter.route("/users").get(getAllUsers); // get All users email
-userRouter.route("/users/create").post(createUser); // get All users email
-userRouter.route("/user/verify").post(sendVerificationToken);
-userRouter.route("/user/verify/:token").get(checkVerificationToken);
+userRouter.route("/users/create").post(createUser); // create user
+userRouter.route("/user/verify").post(sendVerificationToken); // send verification token
+userRouter.route("/user/verify/:token").get(checkVerificationToken); // check the token is valid or not
 
 module.exports = userRouter;
